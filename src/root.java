73bfb6df0ef46abbf13d0ef3950a85538ae54826
Undrown <undrown.off@gmail.com>
2019-04-17 15:51:55 +0300

    start

--- src/root.java
+++ src/root.java
@@ -1,49 +1,43 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.*;
+import java.awt.event.*;
 
 public class root {
     private JPanel rootPanel;
-    private JPanel pixelInfoPanel;
     private JTextField refLabel;
     private JTextField xLabel;
     private JTextField yLabel;
     private JTextField colorLabel;
     private JButton startButton;
     private javax.swing.Timer timer;
-    private final int tps = 10;
+    private Point point;
+    private java.awt.Robot robot;
+    private java.awt.Color color;
+    private final int tps = 100;
 
     public root() {
+        try {robot = new Robot();}
+        catch (Exception e){
+            System.out.println("Error with Robot...");
+        }
+        point = new Point(1,1);
         timer = new javax.swing.Timer((int)(1000/tps), new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                System.out.println("tick");
+                //System.out.println("tick");
+                point = MouseInfo.getPointerInfo().getLocation();
+                color = robot.getPixelColor(point.x, point.y);
+                String color_string = String.format(
+                        "R:%03d   G:%03d   B:%03d",
+                        color.getRed(),
+                        color.getGreen(),
+                        color.getBlue());
+                xLabel.setText(String.valueOf(point.x));
+                yLabel.setText(String.valueOf(point.y));
+                colorLabel.setText(color_string);
             }
         });
         timer.start();
-        rootPanel.addKeyListener(new KeyListener() {
-            @Override
-            public void keyTyped(KeyEvent e) {
-                if (e.getKeyChar() == '1'){
-                    System.out.println("1!!!");
-                }
-                if (e.getKeyChar() == '2'){
-                    System.out.println("2!!!");
-                }
-                System.out.println(e.getID());
-            }
 
-            @Override
-            public void keyPressed(KeyEvent e) {
-
-            }
-
-            @Override
-            public void keyReleased(KeyEvent e) {
-
-            }
-        });
         startButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -57,6 +51,13 @@ public class root {
 
             }
         });
+        rootPanel.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                System.out.println("key");
+            }
+        });
+
     }
 
     public static void main(String[] args) {
@@ -65,5 +66,7 @@ public class root {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+        frame.setAlwaysOnTop(true);
+
     }
 }
